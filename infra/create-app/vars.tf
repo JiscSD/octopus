52973e42d3bbf1f3f2756308e1302ee3e99ced6a
Ashley Redman <ashley.redman@jisc.ac.uk>
2022-01-05 13:09:01 +0000

    Added app & ci providers, main & example tfvars
    int & prod TF vars are not added into the repo as they contain sensitive information.

---
+++ infra/create-app/vars.tf
@@ -0,0 +1,27 @@
+variable "full_domain_name" {
+  type = string
+}
+
+variable "allocated_storage" {
+  type = number
+}
+
+variable "storage_type" {
+  type = string
+}
+
+variable "instance" {
+  type = string
+}
+
+variable "publicly_accessible" {
+  type = bool
+}
+
+variable "acm_certificate_arn" {
+  type = string
+}
+
+variable "rds_identifier" {
+  type = string
+}
