a1bcf53f1cee7ff68441205e486f955e41402882
nathan-at-jisc <nathan.sainsbury@jisc.ac.uk>
2022-01-16 12:24:02 +0000

    more stuff

--- api/src/middleware/authentication.ts
+++ api/src/middleware/authentication.ts
@@ -8,6 +8,17 @@ const authentication = (): middy.MiddlewareObj => {
     const before: middy.MiddlewareFn<I.APIGatewayProxyEventV2> = async (request): Promise<I.JSONResponse | void> => {
         const { Authorization } = request.event.headers;
 
+        const authType = Authorization?.split(' ');
+        let user;
+
+        if (authType[0] === 'Bearer') {
+
+        } else if (authType[0] === 'Basic') {
+
+        } else {
+            return response.json(401, { message: 'Send either a bearer token or API key to authenticate' });
+        }
+
         const token = Authorization?.slice(7, Authorization.length);
         if (!token) {
             return response.json(401, { message: 'No token supplied.' });
