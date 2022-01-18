f41bb7b1db9cdaece112f57d4a50a6419637e557
Ashley Redman <ashley.redman@jisc.ac.uk>
2022-01-18 10:20:49 +0000

    Added tailwind@latest & eslint config

---
+++ ui/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+    return <p>Index</p>;
+};
+
+export default Home;
