e44a1c28090f00a1768c5f09192f36702b1f56de
Nathan Sainsbury <58425475+nathan-at-jisc@users.noreply.github.com>
2022-01-18 21:58:10 +0000

    Add some tests for getting a publication

--- api/src/components/publication/__tests__/getPublication.ts
+++ api/src/components/publication/__tests__/getPublication.ts
@@ -7,20 +7,27 @@ beforeEach(async () => {
 
 describe('View individual publications', () => {
     test('User who created publication can see DRAFT publication', async () => {
-        const getPublication = await testUtils.agent.post('/publications/publication-1').query({
+        const getPublication = await testUtils.agent.get('/publications/publication-1').query({
             apiKey: '123456789'
         });
 
-        expect(getPublication.body.id).toEqual('publication-1')
+        console.log(getPublication.body);
+        expect(getPublication.body.id).toEqual('publication-1');
     });
 
     test('User who did not create publication cannot see DRAFT publication', async () => {
-        const getPublication = await testUtils.agent.post('/publications/publication-1').query({
+        const getPublication = await testUtils.agent.get('/publications/publication-1').query({
             apiKey: '987654321'
         });
 
-        expect(getPublication.body).toBeNull()
+        expect(getPublication.body).toEqual({})
     });
 
+    test('Cannot view publication in DRAFT without API key', async () => {
+        const getPublication = await testUtils.agent.get('/publications/publication-1').query({
+            apiKey: '987654321'
+        });
 
+        expect(getPublication.body).toEqual({})
+    });
 });
