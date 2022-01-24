705132c80595c691dabe3a9620f0529e431da060
Ashley Redman <ashley.redman@jisc.ac.uk>
2022-01-24 11:29:16 +0000

    Cleaned up errors preventing next build

--- ui/src/templates/Error.tsx
+++ ui/src/templates/Error.tsx
@@ -10,9 +10,9 @@ const ErrorTemplate: FC<Props> = (props) => {
         <Layouts.Standard>
             <Components.Section
                 id='error'
-                className='bg-purple-100 dark:bg-grey-600'
-                wave={true}
-                waveFill='fill-purple-100 dark:fill-grey-600'
+                waveFillTop='fill-teal-100 dark:fill-grey-500 transition-colors duration-500'
+                waveFillMiddle='fill-teal-200 dark:fill-grey-600 transition-colors duration-500'
+                waveFillBottom='fill-teal-300 dark:fill-grey-900 transition-colors duration-500'
             >
                 <div className='container mx-auto px-8'>
                     <h1 className='block text-xl'>
