83f158125125eca6a1044841809528c56a981632
Ashley Redman <ashley.redman@jisc.ac.uk>
2022-01-26 13:22:18 +0000

    Cleaned up return type

--- ui/src/components/Video/HTML/index.tsx
+++ ui/src/components/Video/HTML/index.tsx
@@ -10,19 +10,17 @@ type Props = {
     width?: number;
 };
 
-const HTML: React.FC<Props> = (props): JSX.Element => {
-    return (
-        <figure aria-label={props.title} role="video" className=" h-fit w-full rounded-lg bg-white p-2">
-            <video controls={props.controls} width={props.width} poster={props.poster} className="rounded-lg">
-                <source src={props.srcWebM} type="video/webm" />
-                <source src={props.srcMp4} type="video/mp4" />
-            </video>
+const HTML: React.FC<Props> = (props): JSX.Element => (
+    <figure aria-label={props.title} role="video" className=" h-fit w-full rounded-lg bg-white p-2">
+        <video controls={props.controls} width={props.width} poster={props.poster} className="rounded-lg">
+            <source src={props.srcWebM} type="video/webm" />
+            <source src={props.srcMp4} type="video/mp4" />
+        </video>
 
-            <figcaption className={`mt-2 block text-right text-sm ${!props.showCaption && 'sr-only'}`}>
-                {props.title}
-            </figcaption>
-        </figure>
-    );
-};
+        <figcaption className={`mt-2 block text-right text-sm ${!props.showCaption && 'sr-only'}`}>
+            {props.title}
+        </figcaption>
+    </figure>
+);
 
 export default HTML;
--- ui/src/components/Video/YouTube/index.tsx
+++ ui/src/components/Video/YouTube/index.tsx
@@ -8,21 +8,19 @@ type Props = {
     height: number;
 };
 
-const YouTube: React.FC<Props> = (props): JSX.Element => {
-    return (
-        <div className="flex h-fit w-full rounded-lg bg-white p-2">
-            <iframe
-                width={props.width}
-                height={props.height}
-                src={`https://www.youtube.com/embed/${props.embedId}`}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen={props.allowFullScreen}
-                title="Embedded youtube"
-                className="rounded-lg"
-            />
-        </div>
-    );
-};
+const YouTube: React.FC<Props> = (props): JSX.Element => (
+    <div className="flex h-fit w-full rounded-lg bg-white p-2">
+        <iframe
+            width={props.width}
+            height={props.height}
+            src={`https://www.youtube.com/embed/${props.embedId}`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen={props.allowFullScreen}
+            title="Embedded youtube"
+            className="rounded-lg"
+        />
+    </div>
+);
 
 export default YouTube;
