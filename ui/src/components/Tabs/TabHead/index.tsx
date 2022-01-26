20127b42f04b655d79e5d6968c9b9a6b5c6bf10f
Ashley Redman <ashley.redman@jisc.ac.uk>
2022-01-26 10:07:07 +0000

    Updated to meet comments

--- ui/src/components/Tabs/TabHead/index.tsx
+++ ui/src/components/Tabs/TabHead/index.tsx
@@ -11,19 +11,15 @@ type Props = {
     set: React.Dispatch<React.SetStateAction<React.ReactNode>>;
 };
 
-const TabHead: React.FC<Props> = (props): JSX.Element => {
-    return (
-        <button
-            className={`mr-4 border-b-4 border-transparent px-4 pt-3 pb-2 font-montserrat text-lg font-medium leading-tight transition-colors duration-500 ${
-                props.tab === props.active
-                    ? 'border-teal-500 bg-teal-100'
-                    : 'bg-grey-100 dark:bg-grey-600 dark:text-white'
-            }`}
-            onClick={() => props.set(props.tab)}
-        >
-            {props.tab.title}
-        </button>
-    );
-};
+const TabHead: React.FC<Props> = (props): JSX.Element => (
+    <button
+        className={`mr-4 border-b-4 border-transparent px-4 pt-3 pb-2 font-montserrat text-lg font-medium leading-tight transition-colors duration-500 ${
+            props.tab === props.active ? 'border-teal-500 bg-teal-100' : 'bg-grey-100 dark:bg-grey-600 dark:text-white'
+        }`}
+        onClick={() => props.set(props.tab)}
+    >
+        {props.tab.title}
+    </button>
+);
 
 export default TabHead;
