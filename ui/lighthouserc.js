74935d4f268364740a9f71f281996b11100187b9
CarlyRich <69975169+CarlyRich@users.noreply.github.com>
2022-01-27 11:33:52 +0000

    add new script command for lighthouse

--- ui/lighthouserc.js
+++ ui/lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
         collect: {
-            startServerCommand: 'npm run start',
+            startServerCommand: 'npm run lighthouse',
             url: ['http://localhost:3000/']
         },
         upload: {
