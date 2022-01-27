ff0a9a2c54dec4582332850bd830255e0c8e0406
CarlyRich <69975169+CarlyRich@users.noreply.github.com>
2022-01-27 10:53:11 +0000

    change to build site for lighthouse rather than run in dev

--- ui/lighthouserc.js
+++ ui/lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
         collect: {
-            startServerCommand: 'npm run dev',
+            startServerCommand: 'npm run start',
             url: ['http://localhost:3000/']
         },
         upload: {
