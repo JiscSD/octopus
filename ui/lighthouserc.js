03eb63444d4eb84da5e36cf7f5f127e408998272
CarlyRich <69975169+CarlyRich@users.noreply.github.com>
2022-01-27 16:12:25 +0000

    add 404 page to lighthouse workflow

--- ui/lighthouserc.js
+++ ui/lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
     ci: {
         collect: {
             startServerCommand: 'npm run start',
-            url: ['http://localhost:3000/']
+            url: ['http://localhost:3000/', 'http://localhost:3000/404']
         },
         upload: {
             target: 'temporary-public-storage'
