c78a94b1515475f0e6a1e38d19a96dcefcc316ef
CarlyRich <69975169+CarlyRich@users.noreply.github.com>
2022-02-03 10:19:47 +0000

    change testutils

--- api/prisma/seeds/index.ts
+++ api/prisma/seeds/index.ts
@@ -1,2 +1,4 @@
 export { default as users } from './users';
 export { default as publications } from './publications';
+export { default as usersNew } from './usersNew';
+export { default as publicationsNew } from './publicationsNew';
