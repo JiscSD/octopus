8bf04122d1657992bb22a892a9bd73fcba682ef2
Ashley Redman <ashley.redman@jisc.ac.uk>
2022-02-14 12:37:22 +0000

    Update index.tsx

--- ui/src/components/Publication/Sidebar/index.tsx
+++ ui/src/components/Publication/Sidebar/index.tsx
@@ -19,7 +19,9 @@ const Sidebar: React.FC<Props> = (props): JSX.Element => (
             <a
                 key={jumpTo.href}
                 href={`#${jumpTo.href}`}
-                className={`mb-1 block w-fit rounded border-transparent py-1 text-grey-800 outline-0 transition-colors duration-500 focus:ring-2 focus:ring-yellow-400 dark:text-grey-100 ${props.linkClassNames}`}
+                className={`mb-1 block w-fit rounded border-transparent py-1 text-grey-800 outline-0 transition-colors duration-500 focus:ring-2 focus:ring-yellow-400 dark:text-grey-100 ${
+                    !!props.linkClassNames && props.linkClassNames
+                }`}
             >
                 {jumpTo.title}
             </a>
