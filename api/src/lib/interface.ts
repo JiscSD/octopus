dc3eb9d379fc5844fe4f8b6f36316a400c3b524e
CarlyRich <69975169+CarlyRich@users.noreply.github.com>
2022-02-15 15:22:08 +0000

    add licence field to create publication endpoint

--- api/src/lib/interface.ts
+++ api/src/lib/interface.ts
@@ -51,9 +51,12 @@ export type PublicationType =
     | 'INTERPRETATION'
     | 'PEER_REVIEW';
 
+export type LicenceType = 'CC_BY' | 'CC_BY_SA' | 'CC_BY_ND' | 'CC_BY_NC' | 'CC_BY_NC_SA' | 'CC_BY_NC_ND';
+
 export interface CreatePublicationRequestBody {
     type: PublicationType;
     title: string;
+    licence?: LicenceType;
     content?: string;
 }
 
