e55cc3ed2399271adc7ea0803bb4475cbc14324d
Ashley Redman <ashley.redman@jisc.ac.uk>
2022-03-02 10:34:05 +0000

    Small copy update that is bettetr than placeholder
    Better suggested copy can be provided.

--- ui/src/pages/login.tsx
+++ ui/src/pages/login.tsx
@@ -77,7 +77,7 @@ const Login: Types.NextPage<Props> = (props): JSX.Element => {
             <main className="flex h-screen w-full flex-col items-center justify-center bg-teal-50 dark:bg-grey-800">
                 <Assets.Logo width={100} height={100} className="block animate-bounce fill-teal-500" />
                 <h1 className="mb-4 block font-montserrat text-lg font-semibold text-grey-800 dark:text-white">
-                    Logging in verification text here...
+                    Logging you into Octopus
                 </h1>
             </main>
         </>
