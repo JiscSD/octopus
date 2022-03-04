82f084b9dbb10d79d5d8d13d685d4baf73f1b8b0
Carly Richardson <69975169+CarlyRich@users.noreply.github.com>
2022-03-04 17:17:45 +0000

    change how links are shown on individual pub page
    Co-Authored-By: Nathan Sainsbury <58425475+nathan-at-jisc@users.noreply.github.com>
    Co-Authored-By: Ashley Redman <ashley.redman@jisc.ac.uk>

--- ui/src/components/List/Item/index.tsx
+++ ui/src/components/List/Item/index.tsx
@@ -6,13 +6,7 @@ type Props = {
 };
 
 const Item: React.FC<Props> = (props): JSX.Element => (
-    <li
-        className={`relative mb-1 flex before:left-0 before:mr-2 before:inline-block before:w-2 before:font-bold before:text-teal-500 before:content-['\\2022'] ${
-            props.className ? props.className : ''
-        }`}
-    >
-        {props.children}
-    </li>
+    <li className={`relative mb-1 flex ${props.className ? props.className : ''}`}>{props.children}</li>
 );
 
 export default Item;
