f23e153868aa30429ec6757b0496a5ddea7e88b1
Carly Richardson <69975169+CarlyRich@users.noreply.github.com>
2022-03-29 11:11:48 +0100

    migration file

--- api/prisma/migrations/20220329101128_coauthors_table/migration.sql
+++ api/prisma/migrations/20220329101128_coauthors_table/migration.sql
@@ -1,14 +1,11 @@
--- AlterTable
-ALTER TABLE "Publication" ADD COLUMN     "coAuthors" TEXT[];
-
 -- CreateTable
 CREATE TABLE "CoAuthors" (
     "id" TEXT NOT NULL,
     "publicationId" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "code" TEXT NOT NULL,
-    "linkedUser" TEXT,
     "confirmedCoAuthor" BOOLEAN NOT NULL DEFAULT false,
+    "linkedUser" TEXT,
 
     CONSTRAINT "CoAuthors_pkey" PRIMARY KEY ("id")
 );
@@ -20,7 +17,7 @@ CREATE UNIQUE INDEX "CoAuthors_code_key" ON "CoAuthors"("code");
 CREATE UNIQUE INDEX "CoAuthors_publicationId_email_key" ON "CoAuthors"("publicationId", "email");
 
 -- AddForeignKey
-ALTER TABLE "CoAuthors" ADD CONSTRAINT "CoAuthors_linkedUser_fkey" FOREIGN KEY ("linkedUser") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "CoAuthors" ADD CONSTRAINT "CoAuthors_linkedUser_fkey" FOREIGN KEY ("linkedUser") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "CoAuthors" ADD CONSTRAINT "CoAuthors_publicationId_fkey" FOREIGN KEY ("publicationId") REFERENCES "Publication"("id") ON DELETE CASCADE ON UPDATE CASCADE;
