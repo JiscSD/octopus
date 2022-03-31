47923e3a5a3d5e97e96d355c0aed0dffce8a0966
Ashley Redman <ashley.redman@jisc.ac.uk>
2022-03-31 14:40:15 +0100

    Updated

--- api/src/lib/client.ts
+++ api/src/lib/client.ts
@@ -3,12 +3,7 @@ import { Client } from '@opensearch-project/opensearch';
 
 export const prisma = new PrismaClient();
 
-// TODO: stage isn't available here, used an env & it worked, but
-// 'stage' set in package.json is only showing on endpoint hit
-const elasticSearchNode =
-    process.env.STAGE === 'local'
-        ? 'http://admin:admin@localhost:9200'
-        : `https://${process.env.ELASTICSEARCH_USER}:${process.env.ELASTICSEARCH_PASSWORD}@${process.env.ELASTICSEARCH_ENDPOINT}`;
+const elasticSearchNode = `${process.env.ELASTICSEARCH_PROTOCOL}://${process.env.ELASTICSEARCH_USER}:${process.env.ELASTICSEARCH_PASSWORD}@${process.env.ELASTICSEARCH_ENDPOINT}`;
 
 export const search = new Client({
     node: elasticSearchNode
