b97d6543e8d3f3376190c9a9129febaeb9f3ec82
Ashley Redman <ashley.redman@jisc.ac.uk>
2022-04-27 14:35:27 +0100

    Temp rules for api

--- api/src/lib/testUtils.ts
+++ api/src/lib/testUtils.ts
@@ -8,7 +8,7 @@ jest.setTimeout(60000);
 export const agent = supertest.agent(`http://0.0.0.0:4003/${process.env.STAGE}/v1`);
 
 export const initialSeed = async (): Promise<void> => {
-    for (let user of seeds.users) {
+    for (const user of seeds.users) {
         await client.prisma.user.create({
             // @ts-ignore
             data: user
@@ -16,7 +16,7 @@ export const initialSeed = async (): Promise<void> => {
     }
 
     // not ideal, but best thing I can do right now. For some reason createMany will not work with provided seed data...
-    for (let publication of seeds.publications) {
+    for (const publication of seeds.publications) {
         await client.prisma.publication.create({
             // @ts-ignore
             data: publication
