4b8d9c223e0d64e1840cd2d7458d6714bf5907d3
Tom Giddings <tom.giddings@jisc.ac.uk>
2022-05-09 11:19:56 +0100

    Update s3 infra with bucket policy

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -8,3 +8,21 @@ resource "aws_s3_bucket_public_access_block" "image_bucket" {
   block_public_acls   = true
   block_public_policy = true
 }
+
+data "aws_iam_policy_document" "allow_public_access" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      aws_s3_bucket.example.arn,
+      "${aws_s3_bucket.image_bucket.id}/*",
+    ]
+  }
+}
