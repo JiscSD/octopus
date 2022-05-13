bc37f387494f318ce7c07336a44ce123453a00bb
Ashley Redman <ashley.redman@jisc.ac.uk>
2022-05-13 12:25:55 +0100

    Update index.tsx

--- ui/src/components/Avatar/index.tsx
+++ ui/src/components/Avatar/index.tsx
@@ -3,20 +3,18 @@ import React from 'react';
 import * as Interfaces from '@interfaces';
 
 type Props = {
-    user: Interfaces.CoreUser;
+    user: Pick<Interfaces.CoreUser, 'firstName' | 'lastName'>;
     className?: string;
 };
 
-const Avatar: React.FC<Props> = (props): React.ReactElement => {
-    return (
-        <div
-            className={`flex h-12 w-12 items-center justify-center rounded-full border-2 border-teal-500 font-montserrat font-semibold leading-none tracking-tighter text-grey-800 transition-colors duration-500 dark:text-white-50 ${
-                props.className ? props.className : ''
-            }`}
-        >
-            {props.user.firstName[0]} {props.user.lastName[0]}
-        </div>
-    );
-};
+const Avatar: React.FC<Props> = (props): React.ReactElement => (
+    <div
+        className={`flex h-12 w-12 items-center justify-center rounded-full border-2 border-teal-500 font-montserrat font-semibold leading-none tracking-tighter text-grey-800 transition-colors duration-500 dark:text-white-50 ${
+            props.className ? props.className : ''
+        }`}
+    >
+        {props.user.firstName[0]} {props.user.lastName[0]}
+    </div>
+);
 
 export default Avatar;
