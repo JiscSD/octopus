7bcb68afe81d0998c5aa377a6b760f5463b9ec62
Tom Giddings <tom.giddings@jisc.ac.uk>
2022-06-10 14:44:23 +0100

    Changes to visualisation styling

--- ui/src/components/Publication/Visulization/index.tsx
+++ ui/src/components/Publication/Visulization/index.tsx
@@ -36,18 +36,23 @@ const Box: React.FC<BoxProps> = (props): React.ReactElement => {
             animate={{ opacity: 1 }}
             className="relative"
         >
-            {/* <span className="absolute -top-1 z-10 h-3 w-full border-t-4 border-teal-50 dark:border-grey-800"></span> */}
             <Components.Link
                 href={`${Config.urls.viewPublication.path}/${props.publication.id}`}
                 className={`
-            ${props.current ? 'sticky top-0 border-teal-600' : 'border-transparent hover:border-teal-600'}
-             relative z-20 block overflow-hidden rounded-md border-2 bg-white-50 px-3 py-2 text-grey-800 shadow transition-colors duration-500 dark:bg-grey-900 dark:text-white-100
+            ${
+                props.current
+                    ? 'sticky top-0 border-teal-600 bg-teal-700 tracking-wide text-white-50 dark:bg-teal-800'
+                    : 'border-transparent bg-teal-100 text-teal-800 hover:border-teal-600 dark:bg-grey-700 '
+            }
+             relative z-20 block overflow-hidden rounded-md border-2 px-3 py-2 text-grey-800 shadow transition-colors duration-500 dark:text-white-100
             `}
             >
                 <>
                     <div className="space-y-1">
                         <span
-                            className="block h-24 text-xs leading-snug xl:h-16 2xl:text-sm"
+                            className={`block h-24 text-xs leading-snug xl:h-16 2xl:text-sm ${
+                                props.current ? 'font-semibold' : 'font-medium'
+                            }`}
                             title={props.publication.title}
                             role="complementary"
                             aria-label={props.publication.title}
@@ -55,11 +60,21 @@ const Box: React.FC<BoxProps> = (props): React.ReactElement => {
                             {Helpers.truncateString(props.publication.title, 45)}
                         </span>
 
-                        <span className="block text-xxs text-grey-600 transition-colors duration-500 dark:text-grey-200 md:text-xs">
+                        <span
+                            className={`${
+                                props.current
+                                    ? 'font-medium text-teal-100'
+                                    : 'text-grey-600 dark:font-medium dark:text-teal-50'
+                            } block text-xxs transition-colors duration-500 md:text-xs`}
+                        >
                             {props.publication.firstName[0]} {props.publication.lastName}
                         </span>
 
-                        <time className="block h-8 text-xxs text-grey-600 transition-colors duration-500 dark:text-grey-200 md:text-xs xl:h-auto">
+                        <time
+                            className={`${
+                                props.current ? 'text-teal-50' : 'text-grey-600'
+                            } block h-8 text-xxs transition-colors duration-500 dark:text-grey-200 md:text-xs xl:h-auto`}
+                        >
                             {Helpers.formatDate(props.publication.publishedDate)}
                         </time>
                     </div>
@@ -93,7 +108,6 @@ const Visulization: React.FC<VisulizationProps> = (props): React.ReactElement =>
         `${Config.endpoints.publications}/${props.id}/links`,
         null,
         {
-            // revalidateOnFocus: false,
             fallback: {
                 data: {}
             }
@@ -148,17 +162,17 @@ const Visulization: React.FC<VisulizationProps> = (props): React.ReactElement =>
     };
 
     return (
-        <section className="scrollbar-vert mb-8 grid h-72 grid-cols-7 gap-x-8 gap-y-2 overflow-y-auto pb-[5px]">
+        <section className="scrollbar-vert mb-8 grid max-h-[32rem] min-h-[22rem] grid-cols-7 gap-y-4 gap-x-8 overflow-y-auto pb-[5px] xl:gap-x-8">
             {Config.values.publicationTypes.map((type) => {
                 return (
                     // We're not including peer reviews in the 'chain'
                     type !== 'PEER_REVIEW' && (
                         <div key={type} className="relative">
-                            <span className="sticky top-0 z-30 mb-4 block h-12 rounded bg-grey-50 p-2 pl-3 font-montserrat text-xs font-semibold text-grey-800 transition-colors duration-500 dark:bg-grey-900 dark:text-white-50 xl:h-14 xl:text-sm 2xl:h-auto">
+                            <span className="sticky top-0 z-30 mb-4 block h-12 bg-teal-50 p-2 pl-3 font-montserrat text-xs font-semibold text-grey-800 transition-colors duration-500 dark:bg-grey-800 dark:text-white-50 xl:h-14 xl:text-sm 2xl:h-auto">
                                 {Helpers.formatPublicationType(type)}
                             </span>
 
-                            <div className="space-y-4">
+                            <div className="space-y-4 p-1">
                                 {/** data && !error === success */}
                                 {data && !error && (
                                     <>
