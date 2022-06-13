dde05ef105e9e135e4ce3a7e82fb23f61309877c
Tom Giddings <tom.giddings@jisc.ac.uk>
2022-06-13 09:23:38 +0100

    remove stray console.log

--- api/src/components/publication/__tests__/getPublicationLinks.ts
+++ api/src/components/publication/__tests__/getPublicationLinks.ts
@@ -23,8 +23,6 @@ describe('Get links from a supplied publication', () => {
             '/publications/publication-hypothesis-live-problem-live/links'
         );
 
-        console.log(getPublication);
-
         expect(getPublication.statusCode).toEqual(200);
         expect(getPublication.body.linkedFromPublications.length).toBeGreaterThan(0);
         expect(getPublication.body.linkedToPublications.length).toBeGreaterThan(0);
