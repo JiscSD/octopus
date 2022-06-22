d8772be4f58da7859fe1264a77230e730588cce1
shaunajisc <100135505+shaunajisc@users.noreply.github.com>
2022-06-22 15:51:42 +0100

    'octopus aims' page

--- ui/src/config/urls.ts
+++ ui/src/config/urls.ts
@@ -224,7 +224,21 @@ const urls = {
         title: 'Get involved with Octopus',
         description: 'Find out how to participate directly in the Octopus project, and view our development roadmap.',
         keywords: ['User community', 'platform development', 'user participation', 'Octopus publishing'],
-        canonical: `${base.host}/user-terms`
+        canonical: `${base.host}/get-involved`
+    },
+    octopusAims: {
+        path: '/octopus-aims',
+        title: 'Octopus: aims and priorities',
+        description:
+            'Learn more about the aims and priorities of the Octopus platform, which is designed to positively disrupt research culture.',
+        keywords: [
+            'Octopus publishing',
+            'research culture',
+            'research assessment',
+            'open access publishing',
+            'open science'
+        ],
+        canonical: `${base.host}/octopus-aims`
     },
     404: {
         title: `404 Not Found - ${base.title}`
