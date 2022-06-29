44aa0a8b0e5893ab5455c155add48e47a44ae58e
abeeken-jisc <89987378+abeeken-jisc@users.noreply.github.com>
2022-06-29 14:43:51 +0100

    Update urls.ts

--- ui/src/config/urls.ts
+++ ui/src/config/urls.ts
@@ -19,7 +19,7 @@ switch (process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF) {
         host = `https://octopus.ac`;
         mediaBucket = `https://${bucketName}.s3.eu-west-1.amazonaws.com`;
         //orcidAppiID = 'APP-I16GNK4VA08WTE9Y';
-        orcidAppiID = 'APP-0Q7JRZQZG3G0M957';
+        orcidAppiID = 'APP-FEKD033IO62K51EQ';
 }
 
 export const base = {
