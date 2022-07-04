cd93c3d3f70d98ff9990f79c27d10d2944e3acfd
Matthew McGowan <matt.mcgowan@jisc.ac.uk>
2022-07-04 13:08:29 +0100

    Added missing datacite env vars. Fixed error with 'resource' rather than 'data' types used in the terraform for the env vars.

--- infra/modules/api/main.tf
+++ infra/modules/api/main.tf
@@ -11,16 +11,38 @@ resource "aws_ssm_parameter" "jwt_secret" {
   value = random_password.jwt_secret.result
 }
 
-resource "aws_ssm_parameter" "orcid_app_id" {
+data "aws_ssm_parameter" "orcid_app_id" {
   name  = "orcid_app_id_${var.environment}_${var.project_name}"
-  type  = "String"
-  value = ""
-  description = "The orcid api client ID, obtained from orcid, and manually updated on AWS per environment after creation through terraform."
+  // Must be manually created before terraform run. It is required for API releases.
+  // The orcid api client id, obtained from orcid.
 }
 
-resource "aws_ssm_parameter" "orcid_secret_key" {
+data "aws_ssm_parameter" "orcid_secret_key" {
   name  = "orcid_secret_key_${var.environment}_${var.project_name}"
-  type  = "String"
-  value = ""
-  description = "The orcid api client secret, obtained from orcid, and manually updated on AWS per environment after creation through terraform."
+  // Must be manually created before terraform run. It is required for API releases.
+  // The orcid api client secret, obtained from orcid."
+}
+
+data "aws_ssm_parameter" "doi_prefix" {
+  name  = "doi_prefix_${var.environment}_${var.project_name}"
+  // Must be manually created before terraform run. It is required for API releases.
+  // The DOI prefix used for minting datacite DOIs.
+}
+
+data "aws_ssm_parameter" "datacite_endpoint" {
+  name  = "datacite_endpoint_${var.environment}_${var.project_name}"
+  // Must be manually created before terraform run. It is required for API releases.
+  // The datacite endpoint used for minting DOIs.
+}
+
+data "aws_ssm_parameter" "datacite_user" {
+  name  = "datacite_user_${var.environment}_${var.project_name}"
+  // Must be manually created before terraform run. It is required for API releases.
+  // The username of the datacite user, used for minting DOIs.
+}
+
+data "aws_ssm_parameter" "datacite_password" {
+  name  = "datacite_password_${var.environment}_${var.project_name}"
+  // Must be manually created before terraform run. It is required for API releases.
+  // The password of the datacite user, used for minting DOIs.
 }
