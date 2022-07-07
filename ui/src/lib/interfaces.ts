7755c2377c4d95b7b84deff77c80fb6886e7e3cd
Carly Richardson <69975169+CarlyRich@users.noreply.github.com>
2022-07-07 14:01:32 +0100

    types

--- ui/src/lib/interfaces.ts
+++ ui/src/lib/interfaces.ts
@@ -64,7 +64,7 @@ export interface CorePublication {
     licence: Types.LicenceType;
     content: string;
     language: Types.Languages;
-    ethicalStatement: string | null;
+    ethicalStatement: string;
     ethicalStatementFreeText: string | null;
 }
 
