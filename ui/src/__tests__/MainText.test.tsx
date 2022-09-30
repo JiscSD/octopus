bc8a9d7bad8022ca5e22a52eedc55622c31611a4
Harry Krautschneider <harry.krautschneider@jisc.ac.uk>
2022-09-30 15:55:31 +0100

    Ran prettier

--- ui/src/__tests__/MainText.test.tsx
+++ ui/src/__tests__/MainText.test.tsx
@@ -3,24 +3,23 @@ import { render, screen } from '@testing-library/react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
-const component = <Components.PublicationCreationMainText/>;
+const component = <Components.PublicationCreationMainText />;
 
-describe('MainText tests', () => {  
-  it('MainText renders without crashing', () => {
-    render(component);
-    expect(screen.getByTestId('main-text')).toBeInTheDocument();
-  });
+describe('MainText tests', () => {
+    it('MainText renders without crashing', () => {
+        render(component);
+        expect(screen.getByTestId('main-text')).toBeInTheDocument();
+    });
 
+    it('MainText renders select options', () => {
+        render(component);
+        const selectElement = screen.getByTestId('main-text-select-element');
+        expect(screen.getByTestId('main-text-select')).toContainElement(selectElement);
+    });
 
-  it('MainText renders select options', () => {
-    render(component);
-    const selectElement = screen.getByTestId('main-text-select-element')
-    expect(screen.getByTestId('main-text-select')).toContainElement(selectElement);
-  });
-
-  it('MainText renders certain language options', () => {
-    render(component);
-    const selectElement = screen.getByTestId('main-text-select-element')
-    expect(screen.getByTestId('main-text-select')).toHaveTextContent('Cree');
-  });
+    it('MainText renders certain language options', () => {
+        render(component);
+        const selectElement = screen.getByTestId('main-text-select-element');
+        expect(screen.getByTestId('main-text-select')).toHaveTextContent('Cree');
+    });
 });
