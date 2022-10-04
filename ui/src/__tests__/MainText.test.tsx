b0fa57b1db0bf5844349d0d1137c8026ca444fff
Harry Krautschneider <harry.krautschneider@jisc.ac.uk>
2022-10-04 14:57:59 +0100

    Adding changed I missed from PR review

--- ui/src/__tests__/MainText.test.tsx
+++ ui/src/__tests__/MainText.test.tsx
@@ -3,22 +3,24 @@ import { render, screen } from '@testing-library/react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
-const component = <Components.PublicationCreationMainText />;
 
 describe('MainText tests', () => {
-    it('MainText renders without crashing', () => {
+    const component = <Components.PublicationCreationMainText />;
+
+    beforeEach(() => {
         render(component);
+    });
+
+    it('MainText renders without crashing', () => {
         expect(screen.getByTestId('main-text')).toBeInTheDocument();
     });
 
     it('MainText renders select options', () => {
-        render(component);
         const selectElement = screen.getByTestId('main-text-select-element');
         expect(screen.getByTestId('main-text-select')).toContainElement(selectElement);
     });
 
     it('MainText renders certain language options', () => {
-        render(component);
         const selectElement = screen.getByTestId('main-text-select-element');
         expect(screen.getByTestId('main-text-select')).toHaveTextContent('Cree');
     });
