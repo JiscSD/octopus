7193990aa7c923ac362897301bdfa912435aaf3d
Harry Krautschneider <harry.krautschneider@jisc.ac.uk>
2022-11-07 15:04:13 +0000

    Required indicator provided with aria role and sidebar toggle given aria label

--- ui/src/components/RequiredIndicator/index.tsx
+++ ui/src/components/RequiredIndicator/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const RequiredIndicator: React.FC = (): React.ReactElement => (
-    <span title="required" aria-label="required" className="text-red-500">
+    <span title="required" aria-label="required" role="tooltip" className="text-red-500">
         *
     </span>
 );
