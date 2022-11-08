96ad90b3b412ec5760130a4634c5348834c3a92a
Harry Krautschneider <harry.krautschneider@jisc.ac.uk>
2022-11-08 10:28:19 +0000

    Updated colours to meet contrast requirements and added tooltip to button icons on linked pub table

--- ui/src/components/Publication/Creation/LinkToEntry/index.tsx
+++ ui/src/components/Publication/Creation/LinkToEntry/index.tsx
@@ -3,6 +3,7 @@ import * as OutlineIcons from '@heroicons/react/outline';
 
 import * as Interfaces from '@interfaces';
 import * as Helpers from '@helpers';
+import * as Components from '@components';
 
 type Props = {
     link: Interfaces.LinkTo;
@@ -20,7 +21,7 @@ const LinkToEntry: React.FC<Props> = (props): React.ReactElement => {
         <tr key={props.link.id}>
             <td className="space-nowrap py-4 pl-4 pr-3 text-sm text-grey-900 transition-colors duration-500 dark:text-white-50 sm:pl-6">
                 <div className="space-y-2">
-                    <span className="font-montserrat text-sm font-medium text-teal-600 transition-colors duration-500 dark:text-teal-400">
+                    <span className="font-montserrat text-sm font-medium text-teal-600 transition-colors duration-500 dark:text-teal-100">
                         {Helpers.formatPublicationType(props.link.publicationToRef.type)}
                     </span>
                     <p className="text-grey-800 transition-colors duration-500 dark:text-white-50">
@@ -37,13 +38,31 @@ const LinkToEntry: React.FC<Props> = (props): React.ReactElement => {
                 </div>
             </td>
             <td className="space-nowrap py-4 px-8 text-center text-sm font-medium text-grey-900 transition-colors duration-500 dark:text-white-50">
-                <button onClick={handleClick} className="rounded-full">
-                    {loading ? (
-                        <OutlineIcons.RefreshIcon className="h-6 w-6 animate-reverse-spin text-teal-600 transition-colors duration-500 dark:text-teal-400" />
-                    ) : (
-                        <OutlineIcons.TrashIcon className="h-6 w-6 text-teal-600 transition-colors duration-500 dark:text-teal-400" />
-                    )}
-                </button>
+                {loading ? (
+                    <Components.IconButton
+                        className="p-2"
+                        title="Delete"
+                        icon={
+                            <OutlineIcons.RefreshIcon
+                                className="h-6 w-6 animate-reverse-spin text-teal-600 transition-colors duration-500 dark:text-teal-400"
+                                aria-hidden="true"
+                            />
+                        }
+                        onClick={handleClick}
+                        />
+                ) : (
+                    <Components.IconButton
+                        className="p-2"
+                        title="Delete"
+                        icon={
+                            <OutlineIcons.TrashIcon
+                                className="h-6 w-6 text-teal-600 transition-colors duration-500 dark:text-teal-400"
+                                aria-hidden="true"
+                            />
+                        }
+                        onClick={handleClick}
+                    />
+                )}
             </td>
         </tr>
     );
