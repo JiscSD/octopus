2e6bec89c7f98d7c41712298ec1e052a85109e33
Greg Birch <greg.birch@jisc.ac.uk>
2022-12-16 11:25:33 +0000

    OCT-458 conditionally adjust padding & margin
    OCT-458 prettier edit

--- ui/src/components/Publication/ContentSection/index.tsx
+++ ui/src/components/Publication/ContentSection/index.tsx
@@ -4,11 +4,15 @@ type Props = {
     id: string;
     title?: string;
     hasBreak?: boolean;
+    isMainText?: boolean;
     children: React.ReactNode;
 };
 
 const ContentSection: React.FC<Props> = (props): React.ReactElement => (
-    <article id={props.id} className={`border-grey-200 py-10 ${props.hasBreak && 'border-b'}`}>
+    <article
+        id={props.id}
+        className={`border-grey-200 py-10 ${props.isMainText && 'lg:pt-0'} ${props.hasBreak && 'border-b'}`}
+    >
         {!!props.title && (
             <h2 className="mb-4 block font-montserrat text-xl font-semibold text-grey-800 transition-colors duration-500 dark:text-white-100">
                 {props.title}
