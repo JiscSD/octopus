a61220fa75623ad4e5a94b91194524668aee4d22
Florin H <florin.holhos@wingravity.com>
2023-01-13 10:31:01 +0200

    fixed api tests

--- api/src/components/coauthor/__tests__/updateCoAuthor.test.ts
+++ api/src/components/coauthor/__tests__/updateCoAuthor.test.ts
@@ -8,7 +8,7 @@ describe('Update co-author status', () => {
 
     test('Co-author updates their status to true', async () => {
         const coAuthor = await testUtils.agent
-            .patch('/publications/publication-hypothesis-draft/coauthor')
+            .patch('/publications/publication-hypothesis-draft/coauthor-confirmation')
             .query({ apiKey: '000000006' })
             .send({
                 confirm: true
@@ -19,10 +19,10 @@ describe('Update co-author status', () => {
 
     test('Co-author updates their status to false', async () => {
         const coAuthor = await testUtils.agent
-            .patch('/publications/publication-problem-draft/coauthor')
+            .patch('/publications/publication-problem-draft/coauthor-confirmation')
             .query({ apiKey: '000000006' })
             .send({
-                confirm: true
+                confirm: false
             });
 
         expect(coAuthor.status).toEqual(200);
@@ -30,7 +30,7 @@ describe('Update co-author status', () => {
 
     test('Cannot update co-author if not a co-author (1)', async () => {
         const coAuthor = await testUtils.agent
-            .patch('/publications/publication-problem-draft/coauthor')
+            .patch('/publications/publication-problem-draft/coauthor-confirmation')
             .query({ apiKey: '123456789' })
             .send({
                 confirm: true
@@ -41,7 +41,7 @@ describe('Update co-author status', () => {
 
     test('Cannot update co-author if not a co-author (2)', async () => {
         const coAuthor = await testUtils.agent
-            .patch('/publications/publication-problem-draft/coauthor')
+            .patch('/publications/publication-problem-draft/coauthor-confirmation')
             .query({ apiKey: '987654321' })
             .send({
                 confirm: true
