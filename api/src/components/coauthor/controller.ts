229b9238fd7395485d4fa0545ab34240ff71c991
Harry Krautschneider <harry.krautschneider@jisc.ac.uk>
2023-01-25 15:25:10 +0000

    Changed how the guard clauses work to make more sense with the function name

--- api/src/components/coauthor/controller.ts
+++ api/src/components/coauthor/controller.ts
@@ -142,14 +142,14 @@ export const link = async (
 ): Promise<I.JSONResponse> => {
     try {
         const publication = await publicationService.get(event.pathParameters.id);
-        
+
         // Does the publication exist?
         if (!publication) {
             return response.json(404, {
                 message: 'This publication does not exist.'
             });
         }
-        
+
         // Is the publication in draft?
         if (publication.currentStatus === 'LIVE') {
             return response.json(403, {
@@ -157,83 +157,83 @@ export const link = async (
             });
         }
 
-        if (event.body.approve) {
-            if (!event.user) {
-                return response.json(403, {
-                    message: 'To link yourself as a co-author, you must be logged in.'
-                });
-            }
+        const coAuthorByEmail = publication.coAuthors.find((author) => author.email === event.body.email);
 
-            // Cannot link user without a verified email address
-            if (!event.user.email) {
-                return response.json(403, {
-                    message: 'To link yourself as a co-author, you must have a verified email address.'
-                });
-            }
-
-            // Cannot link user to co-author if it is the owner
-            if (publication.user.id === event.user.id) {
-                return response.json(404, {
-                    message: 'You cannot link yourself as the co-author, if you are the creator.'
-                });
-            }
-
-            // User is already linked as a co-author
-            if (publication.coAuthors.some((coAuthor) => coAuthor.linkedUser === event.user?.id)) {
-                return response.json(404, {
-                    message: 'You are already linked as an author on this draft'
-                });
-            }
-
-            // email has already been linked
-            const coAuthorByEmail = publication.coAuthors.find((author) => author.email === event.body.email);
-
+        if (!event.body.approve) {
             if (!coAuthorByEmail) {
                 return response.json(404, {
                     message: 'You are not currently listed as an author on this draft'
                 });
             }
-
+    
             if (coAuthorByEmail.linkedUser) {
                 return response.json(404, {
-                    message: 'User has already been linked to this publication.'
+                    message:
+                        'You have previously verified your involvement. Please contact the submitting author to be removed from this publication.'
                 });
             }
-
-            await coAuthorService.linkUser(event.user.id, event.pathParameters.id, event.body.email, event.body.code);
-
-            return response.json(200, 'Linked user account');
+    
+            await coAuthorService.removeFromPublication(event.pathParameters.id, event.body.email, event.body.code);
+    
+            // notify main author about rejection
+            await email.notifyCoAuthorRejection({
+                coAuthor: {
+                    email: event.body.email
+                },
+                publication: {
+                    title: publication.title || '',
+                    authorEmail: publication.user.email || ''
+                }
+            });
+    
+            return response.json(200, 'Removed co-author from publication');
         }
 
-        // email has already been linked
-        const coAuthorByEmail = publication.coAuthors.find((author) => author.email === event.body.email);
+        if (!event.user) {
+            return response.json(403, {
+                message: 'To link yourself as a co-author, you must be logged in.'
+            });
+        }
 
+        // Cannot link user without a verified email address
+        if (!event.user.email) {
+            return response.json(403, {
+                message: 'To link yourself as a co-author, you must have a verified email address.'
+            });
+        }
+
+        // Cannot link user to co-author if it is the owner
+        if (publication.user.id === event.user.id) {
+            return response.json(404, {
+                message: 'You cannot link yourself as the co-author, if you are the creator.'
+            });
+        }
+
+        // User is already linked as a co-author
+        if (publication.coAuthors.some((coAuthor) => coAuthor.linkedUser === event.user?.id)) {
+            return response.json(404, {
+                message: 'You are already linked as an author on this draft'
+            });
+        }
+
+        // User is no longer linked
         if (!coAuthorByEmail) {
             return response.json(404, {
                 message: 'You are not currently listed as an author on this draft'
             });
         }
 
+        // email has already been linked
         if (coAuthorByEmail.linkedUser) {
             return response.json(404, {
-                message: 'You have previously verified your involvement. Please contact the submitting author to be removed from this publication.'
+                message: 'User has already been linked to this publication.'
             });
         }
 
-        await coAuthorService.removeFromPublication(event.pathParameters.id, event.body.email, event.body.code);
+        await coAuthorService.linkUser(event.user.id, event.pathParameters.id, event.body.email, event.body.code);
 
-        // notify main author about rejection
-        await email.notifyCoAuthorRejection({
-            coAuthor: {
-                email: event.body.email
-            },
-            publication: {
-                title: publication.title || '',
-                authorEmail: publication.user.email || ''
-            }
-        });
-
-        return response.json(200, 'Removed co-author from publication');
+        return response.json(200, 'Linked user account');
+      
     } catch (err) {
         return response.json(500, { message: 'Unknown server error.' });
     }
