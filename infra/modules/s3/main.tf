d64119285fdf368c997b448ce7ac126ece0649d0
Harry Krautschneider <harry.krautschneider@jisc.ac.uk>
2023-03-20 09:29:57 +0000

    Small fix that ensures the tf will run

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -28,5 +28,5 @@ data "aws_iam_policy_document" "allow_public_access" {
 
 resource "aws_s3_bucket_policy" "allow_public_access" {
   bucket = aws_s3_bucket.image_bucket.id
-  policy = data.aws_iam_policy_document.allow_public_access.json
+  policy = data.aws_iam_policy_document.allow_public_access[each.key].json
 }
