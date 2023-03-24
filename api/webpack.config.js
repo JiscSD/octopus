783de30240d94f648659b4c6a7ab3f0df976c3b3
florin-holhos <48569671+florin-holhos@users.noreply.github.com>
2023-03-24 16:17:29 +0200

    OCT-583 Styling PDFs - FIX - added fonts and logo into the bundle when deploying functions (#360)
    * added api tests for pdf generation endpoint
    
    * updated chromium executablePath depending on STAGE
    
    * removed 'getPDF' tests
    
    * apply stash
    
    * fixed assets folder not bundled when deploying functions
    
    ---------
    
    Co-authored-by: Florin H <florin.holhos@wingravity.com>

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const slsw = require('serverless-webpack');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
@@ -21,8 +22,15 @@ module.exports = {
             {
                 test: /\.(tsx?)$/,
                 loader: 'ts-loader',
-                exclude: [[path.resolve(__dirname, 'node_modules'), path.resolve(__dirname, '.serverless'), path.resolve(__dirname, '.webpack')]]
+                exclude: [
+                    [
+                        path.resolve(__dirname, 'node_modules'),
+                        path.resolve(__dirname, '.serverless'),
+                        path.resolve(__dirname, '.webpack')
+                    ]
+                ]
             }
         ]
-    }
+    },
+    plugins: [new CopyPlugin({ patterns: [{ from: 'assets', to: 'assets' }] })]
 };
