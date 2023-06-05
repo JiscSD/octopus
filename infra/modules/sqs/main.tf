3ee8c6699867d5b6a3e8c119e9bf3a2b9fbea42a
Harry Krautschneider <harry.krautschneider@jisc.ac.uk>
2023-06-05 11:24:16 +0100

    Added in some required options

--- infra/modules/sqs/main.tf
+++ infra/modules/sqs/main.tf
@@ -6,6 +6,7 @@ resource "aws_sqs_queue" "science-octopus-pdf-queue" {
   max_message_size              = 2048
   message_retention_seconds     = 86400
   receive_wait_time_seconds     = 0
+  sqs_managed_sse_enabled       = false
   
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.science-octopus-pdf-queue-dlq.arn
@@ -16,6 +17,7 @@ resource "aws_sqs_queue" "science-octopus-pdf-queue" {
 
 resource "aws_sqs_queue" "science-octopus-pdf-queue-dlq" {
   name = "science-octopus-pdf-queue-dlq-${var.environment}"
+  sqs_managed_sse_enabled = false
 }
 
 resource "aws_cloudwatch_metric_alarm" "dlq-messages-sent-alarm" {
