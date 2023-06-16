f91d6cde4a5662bfa48f7f721bfdc3ddc9e97743
Florin H <florin.holhos@wingravity.com>
2023-06-16 12:06:16 +0300

    upgraded to next 13

--- ui/src/components/Footer/index.tsx
+++ ui/src/components/Footer/index.tsx
@@ -97,7 +97,13 @@ const Footer: React.FC<Props> = (props: Props): React.ReactElement => (
                     </h4>
                     <div className="flex">
                         <Components.Link href="https://www.ukri.org/" openNew={true} className="flex w-fit">
-                            <Image src="/images/logo-ukri.png" alt="UKRI" width={200} height={64} />
+                            <Image
+                                src="/images/logo-ukri.png"
+                                alt="UKRI"
+                                width={200}
+                                height={64}
+                                style={{ width: 200, height: 64 }} // removes strange next13 console warning...
+                            />
                         </Components.Link>
                     </div>
                 </div>
