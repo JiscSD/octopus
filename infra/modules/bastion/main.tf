522da3f8da5bfb40f479c32419fcf59cabc63033
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2023-06-29 09:21:29 +0100

    allow SSM access to bastion ec2

--- infra/modules/bastion/main.tf
+++ infra/modules/bastion/main.tf
@@ -1,8 +1,8 @@
 resource "aws_eip" "elastic_ip" {
-  vpc                       = true
-  tags                      =  {
-  Name = "bastion_${var.environment}"
-                     }
+  domain = "vpc"
+  tags   =  {
+    Name = "bastion_${var.environment}"
+  }
 }
 
 resource "aws_eip_association" "eip_assoc" {
@@ -43,13 +43,44 @@ resource "aws_security_group" "ec2_sg" {
     }
 }
 
+resource "aws_iam_role" "allow_ssm_role" {
+  name               = "ssm-role"
+  description        = "Allow SSM access to EC2"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Principal": {"Service": "ec2.amazonaws.com"},
+    "Action": "sts:AssumeRole"
+  }
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "allow_ssm_iam_profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.allow_ssm_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "allow_ssm_iam_policy" {
+  role       = aws_iam_role.allow_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+data "template_file" "user_data" {
+  template = file("./scripts/install-bastion-software.yaml")
+}
+
 resource "aws_instance" "bastion" {
     ami                         = "ami-0bb3fad3c0286ebd5"
-    instance_type               = "t3.small"
+    instance_type               = "t3.nano"
     subnet_id                   = var.public_subnet
     vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
     key_name                    = var.ec2_key_name
     associate_public_ip_address = true
+    user_data                   = data.template_file.user_data.rendered
+    iam_instance_profile        = aws_iam_instance_profile.allow_ssm_iam_profile.name
     
     tags = {
         Name        = "bastion_${var.environment}"
