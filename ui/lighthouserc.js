6f97db146d63b701a2ac969ed1ff0c6685e47e9d
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2023-07-13 16:06:41 +0100

    use npm run dev, not start, for lighthouse

--- ui/lighthouserc.js
+++ ui/lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
         collect: {
-            startServerCommand: 'npm run start',
+            startServerCommand: 'npm run dev',
             url: [
                 'http://localhost:3000/',
                 'http://localhost:3000/browse',
