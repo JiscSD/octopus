1489a0d40bd244a0dcb4ff7792b2ef7044d3a7a3
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2023-08-01 08:54:44 +0100

    specify AMI architecture to match instance type (t3.small)

--- infra/modules/bastion/main.tf
+++ infra/modules/bastion/main.tf
@@ -56,6 +56,11 @@ data "aws_ami" "amazon-linux-2023" {
     name   = "name"
     values = ["al2023-ami-2023*"]
   }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
 }
 
 resource "aws_instance" "bastion" {
