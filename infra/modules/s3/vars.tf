4d89f3633d4b815b0388a6395a8933898e638c00
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2023-08-07 08:29:52 +0100

    mark pubrouter vars as sensitive in s3 module

--- infra/modules/s3/vars.tf
+++ infra/modules/s3/vars.tf
@@ -4,8 +4,10 @@ variable "environment" {
 
 variable "pub_router_failure_channel" {
   type = string
+  sensitive = true
 }
 
 variable "pub_router_api_key" {
   type = string
+  sensitive = true
 }
