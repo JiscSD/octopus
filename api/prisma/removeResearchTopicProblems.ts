de6e730f5d11351a8e929d87b3b027125fff1dff
Florin H <florin.holhos@wingravity.com>
2023-08-22 14:05:06 +0300

    added DOI handler

--- api/prisma/removeResearchTopicProblems.ts
+++ api/prisma/removeResearchTopicProblems.ts
@@ -1,7 +1,32 @@
 import * as client from 'lib/client';
 import * as publicationService from 'components/publication/service';
+import axios from 'axios';
 
-const removeResearchTopicProblems = async (): Promise<number> => {
+const updateDoiState = (doi: string): Promise<unknown> => {
+    console.log('Updating DOI:', doi);
+
+    // registered/findable DOIs cannot be deleted
+    // but we can update their state from "findable" to "registered" so they are not available anymore via the Public API
+    // https://support.datacite.org/docs/doi-states
+    return axios.put(
+        `${process.env.DATACITE_ENDPOINT}/${doi}`,
+        {
+            data: {
+                attributes: {
+                    event: 'hide'
+                }
+            }
+        },
+        {
+            auth: {
+                username: process.env.DATACITE_USER as string,
+                password: process.env.DATACITE_PASSWORD as string
+            }
+        }
+    );
+};
+
+const removeResearchTopicProblems = async (): Promise<{ deletedCount: number; remainingCount: number }> => {
     // get all research topic problems which don't have hypothesis children
     const researchTopicProblems = await publicationService.getResearchTopics({
         linkedFrom: {
@@ -13,19 +38,53 @@ const removeResearchTopicProblems = async (): Promise<number> => {
         }
     });
 
+    const toBeDeleted: string[] = [];
+
+    // update DOIs in chunks
+    const chunkSize = 10;
+
+    for (let i = 0; i < researchTopicProblems.length; i += chunkSize) {
+        const currentChunk = researchTopicProblems.slice(i, i + chunkSize);
+
+        const promises = currentChunk.map((problem) =>
+            updateDoiState(problem.doi).catch((error) => {
+                console.log('Error updating DOI:', problem.doi);
+
+                return error as Error;
+            })
+        );
+
+        const results = await Promise.all(promises);
+
+        results.forEach((result, index) => {
+            if (!(result instanceof Error)) {
+                toBeDeleted.push(currentChunk[index].id);
+            }
+        });
+    }
+
     // delete all research problems where id matches
     // all their links will be deleted in cascade
-    const { count } = await client.prisma.publication.deleteMany({
+    const { count: deletedCount } = await client.prisma.publication.deleteMany({
         where: {
             id: {
-                in: researchTopicProblems.map((problem) => problem.id)
+                in: toBeDeleted
             }
         }
     });
 
-    return count;
+    const remainingCount = researchTopicProblems.length - deletedCount;
+
+    return { deletedCount, remainingCount };
 };
 
 removeResearchTopicProblems()
-    .then((deletedProblemsCount) => console.log(`Successfully deleted ${deletedProblemsCount} research problems`))
+    .then(({ deletedCount, remainingCount }) => {
+        console.log(`Successfully deleted ${deletedCount} research problems.`);
+
+        if (remainingCount) {
+            console.log('Remaining research problems which could not be deleted:', remainingCount);
+            console.log('Please check the logs above.');
+        }
+    })
     .catch((error) => console.log(error));
