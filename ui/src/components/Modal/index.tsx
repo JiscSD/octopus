540abea77eab9e970a79a53333faf150bac84ad7
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2023-10-09 10:49:25 +0100

    put size of modals back to normal

--- ui/src/components/Modal/index.tsx
+++ ui/src/components/Modal/index.tsx
@@ -45,6 +45,7 @@ const Modal: React.FC<Props> = (props) => {
                         &#8203;
                     </span>
                     <HeadlessUI.Transition.Child
+                        as={React.Fragment}
                         enter="ease-out duration-300"
                         enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         enterTo="opacity-100 translate-y-0 sm:scale-100"
