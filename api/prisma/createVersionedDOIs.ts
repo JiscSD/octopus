21f3eac750635b2e38ee85e11530873b2ad60bf2
florin-holhos <48569671+florin-holhos@users.noreply.github.com>
2023-11-03 15:17:40 +0200

    OC-389 Versioned DOIs (#527)
    * finished script, API and UI work
    
    * fixed ts issue after rebase
    
    * fixed alias paths when running seed
    
    * fixed createVersionedDOIs script
    
    ---------
    
    Co-authored-by: Florin H <florin.holhos@wingravity.com>

--- api/prisma/createVersionedDOIs.ts
+++ api/prisma/createVersionedDOIs.ts
@@ -1,14 +1,16 @@
-import * as client from '../lib/client';
-import * as helpers from '../lib/helpers';
+import * as client from '../src/lib/client';
+import * as helpers from '../src/lib/helpers';
+import * as publicationVersionService from '../src/components/publicationVersion/service';
 
-/**
- * This script will update canonical DOIs with their latest versions
- */
-
-const updatePublicationDOIs = async (): Promise<void> => {
+const createVersionedDOIs = async (): Promise<number> => {
+    // get the latest LIVE version of each publication
     const latestLiveVersions = await client.prisma.publicationVersion.findMany({
         where: {
-            isLatestLiveVersion: true
+            isLatestLiveVersion: true,
+            createdBy: {
+                not: 'octopus' // ignore seed data publications
+            },
+            doi: null // only get versions without DOI
         },
         include: {
             publication: {
@@ -79,14 +81,39 @@ const updatePublicationDOIs = async (): Promise<void> => {
         }
     });
 
-    let index = 1;
+    console.log(`Found ${latestLiveVersions.length} without a DOI.`);
+
+    let createdVersionDOIsCount = 0;
 
     for (const version of latestLiveVersions) {
-        await helpers.updatePublicationDOI(version.publication.doi, version).catch((err) => console.log(err));
+        // create a new DOI for each version
+        console.log(`Creating version ${version.versionNumber} DOI for publication ${version.versionOf}`);
+        const versionDOIResponse = await helpers.createPublicationVersionDOI(version);
+        const versionDOI = versionDOIResponse.data.attributes.doi;
 
-        console.log(`No: ${index}. ${version.title} doi updated (${version.publication.doi})`);
-        index++;
+        console.log(`Successfully created version ${version.versionNumber} DOI: ${versionDOI}`);
+
+        // update version DOI
+        console.log(`Updating version DOI: ${versionDOI} in DB`);
+        const updatedVersion = await publicationVersionService.update(version.id, {
+            doi: versionDOI
+        });
+
+        // update publication "HasVersion" with the created DOI
+        console.log(`Updating canonical DOI: ${version.publication.doi} "HasVersion" with version DOI: ${versionDOI}`);
+        await helpers.updatePublicationDOI(version.publication.doi, updatedVersion);
+
+        console.log(`Successfully updated canonical DOI: ${version.publication.doi}`);
+
+        createdVersionDOIsCount += 1;
+        console.log(); // new line
     }
+
+    return createdVersionDOIsCount;
 };
 
-updatePublicationDOIs().catch((err) => console.log(err));
+createVersionedDOIs()
+    .then((versionedDOIsCount) =>
+        console.log(`Successfully created ${versionedDOIsCount} versioned DOIs and updated their canonical DOIs`)
+    )
+    .catch((err) => console.log(err));
