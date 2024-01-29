484f9c09f6534b787b0d075febb12c764c5410c2
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2024-01-29 13:12:54 +0000

    destructure props

--- ui/src/components/AdditionalInformationCard/index.tsx
+++ ui/src/components/AdditionalInformationCard/index.tsx
@@ -8,23 +8,25 @@ type Props = {
     };
 };
 
-const AdditionalInformationCard: React.FC<Props> = (props): React.ReactElement => (
+const AdditionalInformationCard: React.FC<Props> = ({
+    additionalInformation: { title, url, description }
+}): React.ReactElement => (
     <section className="w-full space-y-4 rounded bg-white-50 px-6 py-6 shadow transition-colors duration-500 dark:bg-grey-700">
         <h3 className="font-montserrat font-bold text-grey-800 transition-colors duration-500 dark:text-white-100">
-            {props.additionalInformation.title}
+            {title}
         </h3>
-        {props.additionalInformation.description && (
+        {description && (
             <p className="leading-relaxed text-grey-800 transition-colors duration-500 dark:text-grey-100">
-                {props.additionalInformation.description}
+                {description}
             </p>
         )}
         <a
-            href={props.additionalInformation.url}
-            title={props.additionalInformation.title}
-            aria-label={props.additionalInformation.title}
+            href={url}
+            title={title}
+            aria-label={title}
             className="block text-sm font-semibold uppercase text-teal-600 underline transition-colors duration-500 dark:text-teal-300"
         >
-            Link to {props.additionalInformation.title}
+            Link to {title}
         </a>
     </section>
 );
