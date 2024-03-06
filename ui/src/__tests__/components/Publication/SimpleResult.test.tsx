a75c15dc6bd7faf894ca9b771764f6c6263f7f7b
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2024-03-06 09:13:17 +0000

    unit tests for SidebarCard General

--- ui/src/__tests__/components/Publication/SimpleResult.test.tsx
+++ ui/src/__tests__/components/Publication/SimpleResult.test.tsx
@@ -1,5 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import { userEvent } from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 
 import * as Components from '@/components';
 import * as Helpers from '@/helpers';
