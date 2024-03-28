90fc922e6ab3fcb68e309974267a2fc772588018
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2024-03-28 12:10:20 +0000

    add Content-Security-Policy header

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+const CSPDirectives = [
+    "default-src 'self';",
+    "script-src 'self' 'unsafe-eval' 'unsafe-inline';",
+    "style-src 'self' 'unsafe-inline';",
+    `connect-src 'self' *.api.octopus.ac http://127.0.0.1:4003 https://api.octopus.ac https://api.ror.org https://cdn.contentful.com;`,
+    'img-src https:;',
+    'media-src https://octopus-promo-video.s3.eu-west-1.amazonaws.com https://www.youtube.com;',
+    "font-src 'self';",
+    "object-src 'none';",
+    "base-uri 'self';",
+    "form-action 'self';",
+    "frame-ancestors 'none';",
+    'upgrade-insecure-requests;'
+];
 const nextConfig = {
     reactStrictMode: true,
 
@@ -13,6 +27,19 @@ const nextConfig = {
                 source: '/documentation/api',
                 destination: 'https://jiscsd.github.io/octopus',
                 permanent: true
+            }
+        ];
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: CSPDirectives.join(' ')
+                    }
+                ]
             }
         ];
     }
