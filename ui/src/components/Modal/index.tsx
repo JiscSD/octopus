07ef0ea26c9003a3b80ee157ed435b268af7a3b0
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2024-05-07 09:09:16 +0100

    make positive action callback optional on modal

--- ui/src/components/Modal/index.tsx
+++ ui/src/components/Modal/index.tsx
@@ -6,8 +6,8 @@ import * as Components from '@/components';
 type Props = {
     open: boolean;
     onClose: any; // state setter
-    positiveActionCallback: any; // state setter
-    positiveButtonText: string;
+    positiveActionCallback?: any; // state setter
+    positiveButtonText?: string;
     negativeActionCallback?: any;
     cancelButtonText: string;
     title: string;
@@ -53,7 +53,7 @@ const Modal: React.FC<Props> = (props) => {
                         leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                         leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     >
-                        <div className="relative mx-8 my-20 inline-block w-11/12 transform overflow-hidden rounded-lg bg-white-50 text-left align-bottom shadow-xl transition-all sm:align-middle lg:max-w-xl">
+                        <div className="relative mx-8 my-20 inline-block w-11/12 transform overflow-hidden rounded-lg bg-white-50 text-left align-bottom shadow-xl transition-all sm:align-middle lg:max-w-3xl xl:max-w-5xl">
                             <Components.ModalBarLoader loading={loading} />
                             <div className="px-4 pb-4 pt-5 sm:px-8 sm:py-6">
                                 <div>
@@ -73,13 +73,15 @@ const Modal: React.FC<Props> = (props) => {
                                     </div>
                                 </div>
                                 <div className="mt-5 flex justify-between space-x-4 sm:mt-6">
-                                    <Components.ModalButton
-                                        onClick={() => props.positiveActionCallback()}
-                                        disabled={loading}
-                                        text={props.positiveButtonText}
-                                        title={props.positiveButtonText}
-                                        actionType="POSITIVE"
-                                    />
+                                    {props.positiveActionCallback && props.positiveButtonText && (
+                                        <Components.ModalButton
+                                            onClick={() => props.positiveActionCallback()}
+                                            disabled={loading}
+                                            text={props.positiveButtonText}
+                                            title={props.positiveButtonText}
+                                            actionType="POSITIVE"
+                                        />
+                                    )}
                                     <Components.ModalButton
                                         onClick={() =>
                                             props.negativeActionCallback
