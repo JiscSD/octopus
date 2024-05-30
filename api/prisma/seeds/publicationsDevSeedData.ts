3f7c7aeb1ef139c738e43b5b56c5bbac11e6fb87
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2024-05-30 10:17:40 +0100

    add another seed crosslink so modal is available

--- api/prisma/seeds/publicationsDevSeedData.ts
+++ api/prisma/seeds/publicationsDevSeedData.ts
@@ -867,7 +867,7 @@ const newPublicationSeeds: Prisma.PublicationCreateInput[] = [
                 id: 'user-1-problem-1-crosslink-1',
                 publicationToId: 'publication-user-1-problem-1-live',
                 createdBy: users.user6,
-                createdAt: '2024-04-10T13:35:00.000Z',
+                createdAt: '2024-04-10T13:31:00.000Z',
                 votes: {
                     create: {
                         createdBy: users.user6,
@@ -921,7 +921,7 @@ const newPublicationSeeds: Prisma.PublicationCreateInput[] = [
                 id: 'user-1-problem-1-crosslink-2',
                 publicationToId: 'publication-user-1-problem-1-live',
                 createdBy: users.user6,
-                createdAt: '2024-04-10T13:34:00.000Z',
+                createdAt: '2024-04-10T13:32:00.000Z',
                 votes: {
                     create: {
                         createdBy: users.user6,
@@ -1050,7 +1050,7 @@ const newPublicationSeeds: Prisma.PublicationCreateInput[] = [
                 id: 'user-1-problem-1-crosslink-4',
                 publicationToId: 'publication-user-1-problem-1-live',
                 createdBy: users.user1,
-                createdAt: '2024-04-10T13:32:00.000Z',
+                createdAt: '2024-04-10T13:34:00.000Z',
                 votes: {
                     createMany: {
                         data: [
@@ -1171,7 +1171,7 @@ const newPublicationSeeds: Prisma.PublicationCreateInput[] = [
                 id: 'user-1-problem-1-crosslink-5',
                 publicationToId: 'publication-user-1-problem-1-live',
                 createdBy: users.user1,
-                createdAt: '2024-04-10T13:31:00.000Z',
+                createdAt: '2024-04-10T13:35:00.000Z',
                 score: 5,
                 votes: {
                     createMany: {
@@ -1234,6 +1234,20 @@ const newPublicationSeeds: Prisma.PublicationCreateInput[] = [
                 publicationToId: 'publication-user-2-analysis-1-live',
                 versionToId: 'publication-user-2-analysis-1-live-v1',
                 draft: false
+            }
+        },
+        crosslinksFrom: {
+            create: {
+                id: 'user-1-problem-1-crosslink-6',
+                publicationToId: 'publication-user-1-problem-1-live',
+                createdBy: users.user6,
+                createdAt: '2024-04-10T13:36:00.000Z',
+                votes: {
+                    create: {
+                        createdBy: users.user6,
+                        vote: true
+                    }
+                }
             }
         },
         versions: {
