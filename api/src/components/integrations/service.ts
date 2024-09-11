63e8ff5d459ff84df648507f7c595cf20e3f0ccb
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2024-09-11 15:26:26 +0100

    add ingest log table and use it to decide when to stop ingesting ARIs

--- api/src/components/integrations/service.ts
+++ api/src/components/integrations/service.ts
@@ -1,15 +1,35 @@
 import axios from 'axios';
 import * as ariUtils from 'lib/integrations/ariUtils';
+import * as ingestLogService from 'ingestLog/service';
+
+const MAX_UNCHANGED_STREAK = 5;
 
 /**
  * Incremental ARI ingest.
  * Paginates through ARI questions from the ARI DB API and handles incoming ARIs.
- * If it encounters MAX_UNCHANGED_STREAK ARIs in a row not requiring changes, it stops.
+ * It stops when both of the following conditions are true:
+ *   - It encounters MAX_UNCHANGED_STREAK ARIs in a row not requiring changes.
+ *   - It encounters an ARI with dateUpdated before the start time of the most
+ *       recent successful ingest (if this start time is available).
  */
 export const incrementalAriIngest = async (): Promise<void> => {
+    // Get most start time of last successful run to help us know when to stop.
+    const mostRecentStart = await ingestLogService.getMostRecentStartTime('ARI');
+
+    if (!mostRecentStart) {
+        console.log(
+            `Unable to get most recent start time. This job will stop when it encounters ${MAX_UNCHANGED_STREAK} unchanged ARIs, regardless of their dateUpdated value.`
+        );
+    }
+
+    // Log start time.
+    const log = await ingestLogService.create('ARI');
+
     // Count sequential unchanged ARIs so that we can stop when the streak hits MAX_UNCHANGED_STREAK.
-    const MAX_UNCHANGED_STREAK = 5;
     let unchangedStreak = 0;
+
+    // Track whether we have passed the last successful import start time.
+    let timeOverlap = false;
 
     // Pagination loop.
     let pageUrl = ariUtils.ariEndpoint;
@@ -22,6 +42,10 @@ export const incrementalAriIngest = async (): Promise<void> => {
         const pageAris = response.data.data;
 
         for (const pageAri of pageAris) {
+            if (mostRecentStart && new Date(pageAri.dateUpdated) < new Date(mostRecentStart)) {
+                timeOverlap = true;
+            }
+
             if (!pageAri.isArchived) {
                 // Create, update, or skip this ARI as appropriate.
                 const handle = await ariUtils.handleIncomingARI(pageAri);
@@ -32,7 +56,8 @@ export const incrementalAriIngest = async (): Promise<void> => {
                     // Success is true and actionTaken is none so there were no changes found.
                     unchangedStreak++;
 
-                    if (unchangedStreak >= MAX_UNCHANGED_STREAK) {
+                    // Quit when we reach MAX_UNCHANGED_STREAK and are not knowingly processing ARIs newer than last ingest.
+                    if (unchangedStreak >= MAX_UNCHANGED_STREAK && (timeOverlap || !mostRecentStart)) {
                         break;
                     }
                 } else {
@@ -41,8 +66,18 @@ export const incrementalAriIngest = async (): Promise<void> => {
                     // Log action taken.
                     console.log(`ARI ${pageAri.questionId} handled successfully with action: ${handle.actionTaken}`);
                     writeCount++;
+
                     // Artificial delay to avoid hitting datacite rate limits with publication creates/updates.
-                    await new Promise((resolve) => setTimeout(resolve, 1000));
+                    // https://support.datacite.org/docs/is-there-a-rate-limit-for-making-requests-against-the-datacite-apis
+                    if (handle.actionTaken === 'create') {
+                        // Datacite is hit twice, to initialise DOI and get publication ID, then update DOI with data.
+                        await new Promise((resolve) => setTimeout(resolve, 1000));
+                    }
+
+                    if (handle.actionTaken === 'update') {
+                        // Datacite is hit once, to update the DOI with changes.
+                        await new Promise((resolve) => setTimeout(resolve, 500));
+                    }
                 }
             }
         }
@@ -51,7 +86,9 @@ export const incrementalAriIngest = async (): Promise<void> => {
         // On the last run this will be undefined but that's fine because we won't need to repeat the loop.
         paginationInfo = response.data.meta.pagination;
         pageUrl = paginationInfo.links.next;
-    } while (pageUrl && unchangedStreak < MAX_UNCHANGED_STREAK);
+    } while (pageUrl && unchangedStreak < MAX_UNCHANGED_STREAK && !timeOverlap);
 
+    // Log end time.
+    await ingestLogService.setEndTime(log.id, new Date());
     console.log(`Update complete. Updated ${writeCount} publication${writeCount !== 1 ? 's' : ''}.`);
 };
