b7f24b089da0e14482c622ecbf62056de40ba798
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2024-11-13 09:32:38 +0000

    return null early

--- ui/src/components/Publication/PublicationPage/HeaderActions/index.tsx
+++ ui/src/components/Publication/PublicationPage/HeaderActions/index.tsx
@@ -14,7 +14,11 @@ type Props = {
 const HeaderActions: React.FC<Props> = (props) => {
     const { user } = Stores.useAuthStore();
 
-    return user ? (
+    if (!user) {
+        return null;
+    }
+
+    return (
         <div className="col-span-8 mb-10 pb-10 flex flex-col md:flex-row gap-4 md:gap-8 border-b border-grey-200">
             {Helpers.linkedPublicationTypes[props.publicationType as keyof typeof Helpers.linkedPublicationTypes].map(
                 (childPublicationType) => {
@@ -38,7 +42,7 @@ const HeaderActions: React.FC<Props> = (props) => {
                 />
             )}
         </div>
-    ) : null;
+    );
 };
 
 export default HeaderActions;
