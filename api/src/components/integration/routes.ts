3ecefccc67f7c5c4ad7d133e8b9fabe2a54a752f
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2024-12-12 15:59:19 +0000

    do api key check in middleware

--- api/src/components/integration/routes.ts
+++ api/src/components/integration/routes.ts
@@ -1,13 +1,17 @@
 import middy from '@middy/core';
 
+import * as Helpers from 'lib/helpers';
 import * as integrationController from 'integration/controller';
 import * as integrationSchema from 'integration/schema';
 import * as middleware from 'middleware';
 
+const triggerAriIngestApiKey = Helpers.checkEnvVariable('TRIGGER_ARI_INGEST_API_KEY');
+
 export const incrementalAriIngest = middy(integrationController.incrementalAriIngest)
     .use(middleware.doNotWaitForEmptyEventLoop({ runOnError: true, runOnBefore: true, runOnAfter: true }))
+    .use(middleware.authentication(false, false, triggerAriIngestApiKey))
     .use(middleware.validator(integrationSchema.incrementalAriIngestHttp, 'queryStringParameters'));
 
-export const triggerECSTask = middy(integrationController.triggerECSTask).use(
-    middleware.doNotWaitForEmptyEventLoop({ runOnError: true, runOnBefore: true, runOnAfter: true })
-);
+export const triggerECSTask = middy(integrationController.triggerECSTask)
+    .use(middleware.doNotWaitForEmptyEventLoop({ runOnError: true, runOnBefore: true, runOnAfter: true }))
+    .use(middleware.authentication(false, false, triggerAriIngestApiKey));
