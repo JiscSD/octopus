76cb1228250ad1a7e56bc8364c6a23e5e5d737d3
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2025-02-27 14:34:34 +0000

    try shell script just for copying katex assets, not swc

--- api/scripts/dev-watch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-swc prisma -d dist --copy-files
-mkdir -p assets/katex/fonts
-cp node_modules/katex/dist/katex.min.css assets/katex/katex.min.css
-cp node_modules/katex/dist/fonts/* assets/katex/fonts
-# Compile/copy and watch src directory
-swc src -d dist --copy-files -w
