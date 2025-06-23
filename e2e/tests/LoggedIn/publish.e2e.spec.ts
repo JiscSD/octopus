d778d74d8d1426c10cdc05d86685971af4257a0b
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2025-06-23 15:25:30 +0100

    replace react-beautiful-dnd with dndkit

--- e2e/tests/LoggedIn/publish.e2e.spec.ts
+++ e2e/tests/LoggedIn/publish.e2e.spec.ts
@@ -803,8 +803,7 @@ test.describe('Publication flow + co-authors', () => {
         // publish the new publication
         await page.locator(PageModel.publish.publishButtonTracker).click();
         await Promise.all([
-            page.waitForNavigation(),
-            page.locator(PageModel.publish.confirmPublishButtonTracker).click()
+            (page.waitForURL('**/versions/latest'), page.locator(PageModel.publish.confirmPublishButtonTracker).click())
         ]);
 
         // check publication title and authors
@@ -860,6 +859,7 @@ test.describe('Publication flow + co-authors', () => {
     });
 
     test('Authors order can be changed', async ({ browser }) => {
+        test.slow();
         const page = await Helpers.users.getPageAsUser(browser);
 
         // create new publication
@@ -876,11 +876,11 @@ test.describe('Publication flow + co-authors', () => {
         // change the order of authors using the keyboard
         await page.locator('span[title="Drag to reorder authors"]').first().focus();
         await page.keyboard.press('Space'); // select first row
-        await page.waitForTimeout(100);
+        await page.waitForTimeout(300);
         await page.keyboard.press('ArrowDown'); // move it down
-        await page.waitForTimeout(100);
+        await page.waitForTimeout(300);
         await page.keyboard.press('Space'); // confirm position
-        await page.waitForTimeout(100);
+        await page.waitForTimeout(300);
 
         // verify authors order again
         await expect(page.locator('table > tbody > tr').first()).toContainText(Helpers.users.user2.email);
@@ -898,8 +898,7 @@ test.describe('Publication flow + co-authors', () => {
         // publish
         await page.locator(PageModel.publish.publishButtonTracker).click();
         await Promise.all([
-            page.waitForNavigation(),
-            page.locator(PageModel.publish.confirmPublishButtonTracker).click()
+            (page.waitForURL('**/versions/latest'), page.locator(PageModel.publish.confirmPublishButtonTracker).click())
         ]);
 
         // check publication title and authors
@@ -1167,8 +1166,7 @@ test.describe('Publication flow + co-authors', () => {
         // publish
         await page.locator(PageModel.publish.publishButtonTracker).click();
         await Promise.all([
-            page.waitForNavigation(),
-            page.locator(PageModel.publish.confirmPublishButtonTracker).click()
+            (page.waitForURL('**/versions/latest'), page.locator(PageModel.publish.confirmPublishButtonTracker).click())
         ]);
 
         // check draft publication controls are not available anymore
@@ -1480,8 +1478,7 @@ test.describe('Publication flow + co-authors', () => {
         await expect(page.locator(PageModel.publish.publishButtonTracker)).toBeEnabled();
         await page.locator(PageModel.publish.publishButtonTracker).click();
         await Promise.all([
-            page.waitForNavigation(),
-            page.locator(PageModel.publish.confirmPublishButtonTracker).click()
+            (page.waitForURL('**/versions/latest'), page.locator(PageModel.publish.confirmPublishButtonTracker).click())
         ]);
 
         // close corresponding author session
