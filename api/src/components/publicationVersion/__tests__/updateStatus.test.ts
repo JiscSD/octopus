e57e79a95873c88599c4de3d86a1d7b69c145d14
Finlay Birnie <finlay.birnie@jisc.ac.uk>
2025-06-24 10:44:55 +0100

    update response code

--- api/src/components/publicationVersion/__tests__/updateStatus.test.ts
+++ api/src/components/publicationVersion/__tests__/updateStatus.test.ts
@@ -231,7 +231,7 @@ describe('Update publication version status', () => {
             .query({ apiKey: '000000005' })
             .send();
 
-        expect(updateStatusResponse.status).toEqual(403);
+        expect(updateStatusResponse.status).toEqual(400);
         expect(updateStatusResponse.body.message).toEqual(
             'Publication is not ready to be LOCKED. Make sure all fields are filled in.'
         );
