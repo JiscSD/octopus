b83a8c49156d0703a0615e694757f5d6739c159b
Daniel Berigoi <32541444+danielberigoi@users.noreply.github.com>
2025-06-26 12:18:05 +0300

    OC-1088 - Merge latest OC-1087

--- api/prisma/migrations/20250626082604_user_last_bulletin/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Notification" DROP CONSTRAINT "Notification_userId_fkey";
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "lastBulletinSentAt" TIMESTAMP(3);
-
--- AddForeignKey
-ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
